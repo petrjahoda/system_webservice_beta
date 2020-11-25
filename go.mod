d97416a9748ff97537f127264afcd1650205f5db
petrjahoda <petrjahoda@protonmail.com>
2020-11-25 08:22:16 +0100

    ## [2020.4.2.25] - 2020-11-25
    ### Added
    - service functionality
    
    Took 3 minutes

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/TwinProduction/go-color v0.0.2
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/kardianos/service v1.2.0
 	github.com/petrjahoda/database v0.0.0-20201117102627-93588a38b1e9
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	gorm.io/driver/postgres v1.0.5
--- main.go
+++ main.go
@@ -2,12 +2,53 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/kardianos/service"
+	"github.com/petrjahoda/database"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
+	"os"
+	"time"
 )
 
+const version = "2020.4.2.20"
+const serviceName = "System WebService"
+const serviceDescription = "System web interface"
 const config = "user=postgres password=Zps05..... dbname=version3 host=localhost port=5432 sslmode=disable"
 
+type program struct{}
+
 func main() {
+	logInfo("MAIN", serviceName+" ["+version+"] starting...")
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: serviceName,
+		Description: serviceDescription,
+	}
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+	if err != nil {
+		logError("MAIN", "Cannot start: "+err.Error())
+	}
+	err = s.Run()
+	if err != nil {
+		logError("MAIN", "Cannot start: "+err.Error())
+	}
+}
+
+func (p *program) Start(service.Service) error {
+	logInfo("MAIN", serviceName+" ["+version+"] started")
+	go p.run()
+	return nil
+}
+
+func (p *program) Stop(service.Service) error {
+	logInfo("MAIN", serviceName+" ["+version+"] stopped")
+	return nil
+}
+
+func (p *program) run() {
+	updateProgramVersion()
 	router := httprouter.New()
 	router.ServeFiles("/js/*filepath", http.Dir("js"))
 	router.ServeFiles("/css/*filepath", http.Dir("css"))
@@ -16,9 +57,32 @@ func main() {
 	router.POST("/get_content", getContent)
 	router.POST("/get_content_data", getContentData)
 	router.POST("/verify_user", verifyUser)
-	_ = http.ListenAndServe(":80", router)
+	err := http.ListenAndServe(":80", router)
+	if err != nil {
+		logError("MAIN", "Problem starting service: "+err.Error())
+		os.Exit(-1)
+	}
+	logInfo("MAIN", serviceName+" ["+version+"] running")
 }
 
 func system(writer http.ResponseWriter, request *http.Request, _ httprouter.Params) {
 	http.ServeFile(writer, request, "./html/system.html")
+}
+
+func updateProgramVersion() {
+	logInfo("MAIN", "Writing program version into settings")
+	timer := time.Now()
+	db, err := gorm.Open(postgres.Open(config), &gorm.Config{})
+	sqlDB, _ := db.DB()
+	defer sqlDB.Close()
+	if err != nil {
+		logError("MAIN", "Problem opening database: "+err.Error())
+		return
+	}
+	var existingSettings database.Setting
+	db.Where("name=?", serviceName).Find(&existingSettings)
+	existingSettings.Name = serviceName
+	existingSettings.Value = version
+	db.Save(&existingSettings)
+	logInfo("MAIN", "Program version written into settings in "+time.Since(timer).String())
 }
